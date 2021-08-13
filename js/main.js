cf5b99bb2b842dd973cb14ef3852cab971e429c7
irinapr25 <88492720+irinapr25@users.noreply.github.com>
2021-08-13 18:14:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(function () {
 			}
 		});
 	});
+
+	$(window).scroll(function() {
+		$('.order__btn').each(function(){
+			var imagePos = $(this).offset().top;
+
+		 	var topOfWindow = $(window).scrollTop();
+		 	if (imagePos < topOfWindow+650) {
+				$(this).addClass('animate__heartBeat');
+			}
+		});
+	});
 });
